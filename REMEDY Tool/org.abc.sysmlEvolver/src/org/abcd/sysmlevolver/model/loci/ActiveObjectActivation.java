e2df77988546d0f5c93674d86bdc76fcb25b321e
ABCRepository <132692540+ABCRepository@users.noreply.github.com>
2024-12-08 20:22:50 +0800

    Update ActiveObjectActivation.java

--- REMEDY Tool/org.abc.sysmlEvolver/src/org/abcd/sysmlevolver/model/loci/ActiveObjectActivation.java
+++ REMEDY Tool/org.abc.sysmlEvolver/src/org/abcd/sysmlevolver/model/loci/ActiveObjectActivation.java
@@ -71,99 +71,15 @@ public class ActiveObjectActivation extends SM_ObjectActivation {
 			this.hasBeenWaiting = true;
 			((AsyncControlDelegate) FUMLExecutionEngine.eInstance.getControlDelegate()).notifyWaitingStateEntered(this);
 		}
-		// System.err.println("--------------------no
-		// empty------------------------");
+		
 		EventOccurrence eventOccurrence = evtPool.getNextEvent();
-		// while(true){
-		// try {
-		// eventOccurrence = this.evtPool.poll(500, TimeUnit.MILLISECONDS);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// if(eventOccurrence == null){
-		// isWaiting = true;
-		// continue;
-		// }
-		// else{
-		// isWaiting = false;
-		// System.out.println("receive " + eventOccurrence);
-		// break;
-		// }
-		// }
+		
 		this.currentState = ObjectActivationState.RUNNING;
 
-		if (eventOccurrence != null) {
-			if (eventOccurrence instanceof SignalEventOccurrence) {
-				AsyncDebug.println("[EVENT CONSUMED] occurrence for a signal:  "
-						+ ((SignalEventOccurrence) eventOccurrence).signalInstance);
-			}
-			if (eventOccurrence instanceof InvocationEventOccurrence) {
-				AsyncDebug.println("[EVENT CONSUMED] occurrence to start a classifier behavior: "
-						+ ((InvocationEventOccurrence) eventOccurrence).execution);
-			}
-			if (eventOccurrence instanceof CallEventOccurrence) {
-				if (operationBeingInvoked != null && ((CallEventOccurrence) eventOccurrence).execution.operation
-						.getName().equals(operationBeingInvoked)) {
-					System.out.println("~~~ process call operation event: " + operationBeingInvoked);
-					operationBeingInvoked = null; // invocation is done
-				}
-			}
-			if (eventOccurrence instanceof ChangeEventOccurrence) {
-				System.out.println("~~~ process change event: " + eventOccurrence.toString());
-			}
-		}
 		return eventOccurrence;
 	}
 
-	@Override
-	public void dispatchNextEvent() {
-
-		/* 1. Get next event is blocking if used on a empty event pool */
-		EventOccurrence eventOccurrence = this.getNextEvent();
-
-		AsyncDebug.println("[dispatchNextEvent] eventOccurrence = " + eventOccurrence);
-		/* 2. Look for EventAccepter that match the selected SignalInstance */
-		List<Integer> matchingEventAccepterIndexes = new ArrayList<Integer>();
-		List<EventAccepter> waitingEventAccepters = this.waitingEventAccepters;
-		for (int i = 0; i < waitingEventAccepters.size(); i++) {
-			EventAccepter eventAccepter = waitingEventAccepters.get(i);
-			if (eventAccepter.match(eventOccurrence)) {
-				matchingEventAccepterIndexes.add(i);
-			}
-		}
-		/* 3. Choose one matching event accepter non-deterministically */
-		if (matchingEventAccepterIndexes.size() > 0) {
-			int j = ((ChoiceStrategy) this.object.locus.factory.getStrategy("choice"))
-					.choose(matchingEventAccepterIndexes.size());
-			EventAccepter selectedEventAccepter = this.waitingEventAccepters
-					.get(matchingEventAccepterIndexes.get(j - 1));
-			// this.waitingEventAccepters.remove(j - 1);
-			this.waitingEventAccepters.remove(selectedEventAccepter);
-			if (this.hasBeenWaiting) {
-				this.hasBeenWaiting = false;
-				if (selectedEventAccepter instanceof AcceptEventActionEventAccepter) {
-					((AsyncControlDelegate) FUMLExecutionEngine.eInstance.getControlDelegate())
-							.notifyWaitingStateExit(this, (AcceptEventActionEventAccepter) selectedEventAccepter);
-				}
-			}
-			System.out.println("~~~ selected accepter " + selectedEventAccepter);
-			selectedEventAccepter.accept(eventOccurrence);
-
-		} else {
-			if (eventOccurrence instanceof CallEventOccurrence) {
-				System.out.println("~~~ event loss " + eventOccurrence);
-				if (!((CallEventOccurrence) eventOccurrence).execution.operation.getName().equals("setRCFail")) {
-					System.out.println("~~~ event loss " + eventOccurrence);
-					for (int i = 0; i < waitingEventAccepters.size(); i++) {
-						EventAccepter eventAccepter = waitingEventAccepters.get(i);
-						if (eventAccepter.match(eventOccurrence)) {
-							matchingEventAccepterIndexes.add(i);
-						}
-					}
-				}
-			}
-			AsyncDebug.printLostSignal(eventOccurrence, this, out);
-		}
+	
 	}
 
 	public String getName() {
