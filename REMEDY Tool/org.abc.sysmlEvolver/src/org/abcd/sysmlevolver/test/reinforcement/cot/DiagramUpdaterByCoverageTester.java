5c85722665a79fbf439ed25e86386450d24b6fea
ABCRepository <132692540+ABCRepository@users.noreply.github.com>
2024-12-09 11:32:36 +0800

    Update DiagramUpdaterByCoverageTester.java

--- REMEDY Tool/org.abc.sysmlEvolver/src/org/abcd/sysmlevolver/test/reinforcement/cot/DiagramUpdaterByCoverageTester.java
+++ REMEDY Tool/org.abc.sysmlEvolver/src/org/abcd/sysmlevolver/test/reinforcement/cot/DiagramUpdaterByCoverageTester.java
@@ -115,7 +115,7 @@ public class DiagramUpdaterByCoverageTester extends DiagramUpdater {
 					Constraint guard = valueSpecificationToConstraint(specification);
 					List<ParameterValue> lastTestInputs = TestInputGenerator.instance().generateTestInput(guard,
 							bestOperation.getOwnedParameters());
-					String msg = sendAndReceiveMsg(lastTestInputs);
+					String msg = "";
 
 					VertexActivation stateActivation = currentStateconfiguration.getVertexActivation();
 					String curStateName = stateActivation.getNode().getName();
